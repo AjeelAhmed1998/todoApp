a3bdb8b73730adb31915583a478c4b65b488570d
AjeelAhmed1998 <ajeelahmed1998@gmail.com>
2020-05-05 14:07:44 +0500

    first version, without mods of my own

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'; 
+import {View, Text, StyleSheet} from 'react-native'; 
+import {styles as GlobalStyles} from '../utils/styles'; 
+
+
+export const Header = (props) => {
+    return(
+        <View style = {styles.header}>
+            <Text style = {styles.headerText}>
+
+            {props.title}
+
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        flex: 1, 
+        height: 122, 
+        textAlign: 'center', 
+        justifyContent: 'center', 
+
+    }, 
+
+    headerText:{
+        fontSize: GlobalStyles.fontSize, 
+        color: GlobalStyles.fontColor, 
+        fontWeight: '800'
+    }
+}); 
---
+++ src/utils/styles.js
@@ -0,0 +1,5 @@
+export const styles = {
+    color: '#ffffff', 
+    fontSize: 20, 
+    appBackgroundColors: ['#4776E6', '#8E54E9']
+}
